4f656896014aab79f2cc32090f6db6b1c1fa32d6
fahadsaleemfd <fahadsaleem164@outlook.com>
2024-11-30 22:00:16 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import { withPayload } from "@payloadcms/next/withPayload";
-import { withPayload } from "@payloadcms/next/withPayload";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
